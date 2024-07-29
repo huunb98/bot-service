027a7d4feb7cb3ec2f739dd27401e1ac4daaa0b7
huu.nb <37497217+huunb98@users.noreply.github.com>
2024-07-29 16:45:38 +0700

    update hook telegram

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,12 @@ app.get("/", (req, res) => {
 
 const telegramBot = new TelegramBotService().getBotService();
 
-app.post("", (req, res) => {
+app.post("/hook/telegram", (req, res) => {
   const message: TelegramBot.Message = req.body.message;
   if (message) telegramBot.onNewUpdate(message);
   res.sendStatus(200);
 });
+
 app.post("/webhook", (req, res) => {
   if (req.body.ref == "refs/heads/master") {
     console.log("master branch updated");
